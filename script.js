dff5c2e3df3fc28eed4a8a309e40d058c163271d
Nabeel Hussain <mnnh2001@gmail.com>
2021-06-10 17:56:05 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav_link');
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav_open');
+});
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav_open');
+    });
+})
